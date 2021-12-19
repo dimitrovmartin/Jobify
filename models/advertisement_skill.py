360b00f3b6179a381261477168d140cf7495c11d
dimitrovmartin <esdiedno@gmail.com>
2021-12-20 00:38:59 +0200

    Refactor

--- models/advertisement_skill.py
+++ models/advertisement_skill.py
@@ -7,8 +7,8 @@ from models import SkillModel, AdvertisementModel
 class AdvertisementSkillModel(db.Model):
     __tablename__ = 'advertisements_skills'
 
-    advertisement_id = db.Column(db.Integer, db.ForeignKey('advertisements.id'), primary_key=True)
-    skill_id = db.Column(db.Integer, db.ForeignKey('skills.id'), primary_key=True)
+    advertisement_id = db.Column(db.Integer, db.ForeignKey('advertisements.id'), primary_key=True, nullable=False)
+    skill_id = db.Column(db.Integer, db.ForeignKey('skills.id'), primary_key=True, nullable=False)
 
     advertisement = relationship(AdvertisementModel, backref=backref("advertisements_skills", cascade="all, delete-orphan"))
     skill = relationship(SkillModel, backref=backref("advertisements_skills", cascade="all, delete-orphan"))
--- models/user_skill.py
+++ models/user_skill.py
@@ -7,8 +7,8 @@ from models import CompanyUserModel, SkillModel
 class UserSkillModel(db.Model):
     __tablename__ = 'users_skills'
 
-    applicant_user_id = db.Column(db.Integer, db.ForeignKey('applicant_users.id'), primary_key=True)
-    skill_id = db.Column(db.Integer, db.ForeignKey('skills.id'), primary_key=True)
+    applicant_user_id = db.Column(db.Integer, db.ForeignKey('applicant_users.id'), primary_key=True, nullable=False)
+    skill_id = db.Column(db.Integer, db.ForeignKey('skills.id'), primary_key=True, nullable=False)
 
     applicant_user = relationship(CompanyUserModel, backref=backref("users_skills", cascade="all, delete-orphan"))
     skill = relationship(SkillModel, backref=backref("users_skills", cascade="all, delete-orphan"))
