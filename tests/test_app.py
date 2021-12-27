2b32b6391d97f69925f0b97b4e3b65946c9d9a6a
dimitrovmartin <esdiedno@gmail.com>
2021-12-27 18:37:28 +0200

    added test for company permissions

--- tests/test_app.py
+++ tests/test_app.py
@@ -53,14 +53,44 @@ class TestApplication(TestCase):
             assert resp.status_code == 400
             assert resp.json == {'message': 'Invalid token!'}
 
+    def test_protected_company_endpoints_require_company_rights(self):
+        url_methods = [
+            ('/advertisements', "POST"),
+            ('/advertisements/1/appliers', "GET"),
+            ('/advertisements/1/appliers/export', "GET"),
+            ('/advertisements/1/delete', "DELETE"),
+            ('/advertisements/1/update', "PUT"),
+            ('/advertisements/1/1/approve', "POST"),
+            ('/advertisements/1/1/reject', "POST"),
+        ]
+
+        applier = ApplicantFactory()
+        token = AuthManager.encode_token(applier)
+        headers = {"Authorization": f"Bearer {token}"}
+
+        for url, method in url_methods:
+
+            if method == "GET":
+                resp = self.client.get(url, headers=headers)
+            elif method == "POST":
+                resp = self.client.post(url, data=json.dumps({}), headers=headers)
+            elif method == "PUT":
+                resp = self.client.put(url, data=json.dumps({}), headers=headers)
+            else:
+                resp = self.client.delete(url, headers=headers)
+
+            assert resp.status_code == 403
+            assert resp.json == {'message': 'You don`t have access to this resource!'}
+
     def test_protected_admin_endpoints_require_admin_rights(self):
         url_methods = [
             ('/registerAdmin', "POST"),
             ('/companyUsers/1/delete', "DELETE"),
             ('/applicantUsers/1/delete', "DELETE"),
         ]
-        complainer = ApplicantFactory()
-        token = AuthManager.encode_token(complainer)
+
+        applier = ApplicantFactory()
+        token = AuthManager.encode_token(applier)
         headers = {"Authorization": f"Bearer {token}"}
 
         for url, method in url_methods:
